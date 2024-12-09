1ccc904b8034aa7f5f52ba382597611a5082dd1d
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2024-12-09 12:58:16 -0500

    added redirects

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,38 @@
 const nextConfig = {
     // output: "export",  // <=== enables static exports
     reactStrictMode: true,
+
+    async redirects() {
+        return [
+            {
+                source: '/clock',
+                destination: '/web-art/clock',
+                permanent: false
+            },
+            {
+                source: '/generative-art',
+                destination: '/web-art/generative-art',
+                permanent: false
+            },
+            {
+                source: '/landscapes',
+                destination: '/web-art/landscapes',
+                permanent: false
+            },
+            {
+                source: '/music-shelf',
+                destination: '/web-art/music-shelf',
+                permanent: false
+            },
+            {
+                source: '/tournament',
+                destination: '/web-art/tournament',
+                permanent: false
+            },
+        ]
+    }
 };
+
+
 
 export default nextConfig;
--- src/app/web-art/page.js
+++ src/app/web-art/page.js
@@ -5,14 +5,17 @@ import "./web-art.css";
 export default function Home() {
   return (
     <div>
-        <ul id="portfolio-list">
+        
+    </div>
+  );
+}
+
+
+{/* <ul id="portfolio-list">
           <li>an <a href="./web-art/music-shelf">interactive album art collage</a></li>
           <li>a collection of my <a href="./web-art/landscapes">landscape photography,</a></li>
           <li>a <a href="./web-art/clock">&apos;game&apos; about wasting time,</a></li>
           <li>my team&apos;s rock-paper-scissors <a href="./web-art/tournament">tournament</a></li>
           <li>my team&apos;s mcdonald&apos;s + slack <a href="https://quinn2liu.github.io/section3assignment2/index.html" target="_blank" rel="noopener noreferrer">IFTTT applet</a></li>
           <li>a <a href="./web-art/generative-art">generative art space</a> to jot down your thoughts</li>
-        </ul>
-    </div>
-  );
-}
+        </ul> */}
