d48666e304986fb13811a185dcc9da5769d35fc3
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2024-12-08 21:23:09 -0500

    generative art mobile notice

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
 
           <li>my team&apos;s rock-paper-scissors <a href="./tournament">tournament</a></li>
 
-          <li>my team&apos;s mcdonald&apos;s + slack <a href="https://quinn2liu.github.io/section3assignment2/index.html" target="_blank" rel="noopener noreferrer">applet</a></li>
+          <li>my team&apos;s mcdonald&apos;s + slack <a href="https://quinn2liu.github.io/section3assignment2/index.html" target="_blank" rel="noopener noreferrer">IFTTT applet</a></li>
 
           <li>a <a href="./generative-art">generative art space</a> to jot down your thoughts</li>
 
