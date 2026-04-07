cc07673043e7e44e146bdc1e327495d140ff6571
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2026-04-06 22:52:08 -0400

    update homepage

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="text-content">
           <p className="title-text">nick cirillo</p>
 
-          <p>I'm a student at the University of Pennsylvania, and soon I'll be working at Northslope Technologies.</p>
+          <p>Hi, I&apos;m Nick! I&apos;m a student at the <a href="https://www.upenn.edu/" target="_blank" rel="noopener noreferrer">University of Pennsylvania</a>, and soon I&apos;ll be working at <a href="https://www.northslope.com/" target="_blank" rel="noopener noreferrer">Northslope</a>.</p>
 
           <p>
             <a href="https://linkedin.com/in/nicholas-cirillo-/" target="_blank" rel="noopener noreferrer">linkedin</a>
@@ -18,21 +18,14 @@ export default function Home() {
           </p>
 
           <p>
-            <a href="https://instagram.com/nick.pics.cirillo/" target="_blank" rel="noopener noreferrer">photography</a> (best viewed on the mobile instagram app)
+            <a href="https://instagram.com/nick.pics.cirillo/" target="_blank" rel="noopener noreferrer">photography</a>
           </p>
 
           <p>
-            <a href="./web-art">web doodles</a> (from penn's dsgn 1020)
+            <a href="./web-art">web doodles</a> (from penn&apos;s dsgn 1020)
           </p>
-
-
-          <div id="social-icons">
-            <a className="social-icon" href="https://linkedin.com/in/nicholas-cirillo-/" target="_blank" rel="noopener noreferrer"><img id="linkedin" src="./social-icons/linkedin.svg" alt="the Linkedin logo" /></a>
-            <a className="social-icon" href="https://github.com/nick-cirillo/" target="_blank" rel="noopener noreferrer"><img id="github" src="./social-icons/github.svg" alt="the GitHub logo" /></a>
-            <a className="social-icon" href="https://instagram.com/nick.pics.cirillo/" target="_blank" rel="noopener noreferrer"><img id="instagram" src="./social-icons/instagram.svg" alt="the Instagram logo" /></a>
-          </div>
         </div>
-        <img className="image" id="timber" src="./homepage/timber.jpg" alt="a photo of Timber, the world's cutest puppy" />
+        <img className="image" id="timber" src="./homepage/timber.jpg" alt="a photo of Timber, the world&apos;s cutest puppy" />
       </div>
     </div>
   );
